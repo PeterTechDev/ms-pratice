e9ec15d118d11a6d0e13a58b82f5e988984ff79c
petertechdev <peterleite.dev@gmail.com>
2022-10-08 07:53:57 -0300

    Implements h2 db

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('LeBron', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Curry', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Tatum', 250.0);
